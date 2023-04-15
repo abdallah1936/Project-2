ed95be5db31ed63adcae0a8c7944b3ab74df4889
abdallah1936 <awadallaabdallah@gmail.com>
2023-04-15 03:03:10 -0700

    working on the users

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,10 @@ const authController = require("../controllers/authController");
 router.get("/login", authController.getLogin);
 router.get("/register", authController.getRegister);
 
+router.post('/register', passport.authenticate('local-signup', {
+  successRedirect: '/users/workouts',
+  failureRedirect: '/register',
+  failureFlash: true,
+}));
+
 module.exports = router;
